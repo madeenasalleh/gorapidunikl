55a9d65287f001378aff9de2c578c24d56008827
madeenasalleh <48944853+madeenasalleh@users.noreply.github.com>
2019-03-27 17:09:01 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+</head>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
+<body>
+
+
+</div>
+	<header>
+			<div class="row">
+		<ul class="main-nav">
+			<li class="active"><a href="homepage.html">HOME</a> </li>
+			<li><a href="">SERVICES</a></li>
+			<li><a href="aboutus.php">ABOUT</a> </li>
+			<li><a href="contactus.php">CONTACT US</a> </li>
+			
+		</ul>
+	</div>	
+	</header>
+	
+	<div class="sectionContainer-a">
+	
+		<h2 class="sectionHeader-a">Getting The Card</h2>
+	
+	</div>
+
+		<div class="row">
+			<ul class="apply">
+			<li class="active"><a href="signupnew.php">Apply Here</a> </li>
+			
+		</div>
+		
+		</div>
+		<div class="row">
+			<ul class="pay">
+			<li class="active"><a href="">Pay Fee</a> </li>
+			
+		</div>
+
+	<div class="sectionContainer-b">
+		<p class="sectionContent"></p>
+		
+	</div>
+
+	<div class="sectionContainer-c">
+		<h1>All-in-one platform</h1>
+		<p>We've got you cover</br>
+		With integrated platform,</br>you can get your card with less hassle and visit us with less time.</br>
+			
+			Feel free to keep in touch with us.<br>
+			 </p>
+
+			 <div><input class="contactusbtn" type="button" value="CONTACT US" onclick="window.location.href='contactus.php'" /></div>
+	
+
+
+</body>
+<footer>
+			<div class="footerWrapper">
+				<div class="footerLogoWrapper"><a href="" class="footerLogoText"></a></div>
+				<div class="footerLegalWrapper">
+				
+				</div>
+				<div class="footerContactWrapper">
+					<p class="footerContactText"></p>
+				</div>
+			</div>
+		</footer>		
+</html>
